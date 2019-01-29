2779648cc7627f96694c8d0fe445b7aa3fd7121b
deepak9738 <deepak.s.m95@gmail.com>
2019-01-29 11:17:12 +0000

    Add files via upload

--- actions.py
+++ actions.py
@@ -1,4 +1,7 @@
 from rasa_core.actions import Action
+from rasa_core_sdk.events import SlotSet
+import httplib2
+
 
 
 class ActionExample(Action):
@@ -9,4 +12,59 @@ class ActionExample(Action):
 
     def run(self, dispatcher, tracker, domain):
         # what your action should do
+        s=str(tracker.get_slot("website"))
+        URL="https://www."+s+".com"
+        try:
+            resp, content = httplib2.Http().request(URL)
+            if resp.status==200:
+                response="""{} is up and running  """.format(s)
+                dispatcher.utter_message(response)
+            else:
+                dispatcher.utter_message("Sorry!")
+        except:
+            dispatcher.utter_message("Sorry! an exception occured in accessing "+s+" website")
         return []
+
+class ActionStartJenkinsBuild(Action):
+    def name(self):
+        return "action_jenkins"
+
+    def run(self, dispatcher, tracker, domain):
+        job = tracker.get_slot("app_name")
+        URL = "http://localhost:8080/job/"+job+"/build?token=remote_enable_token"
+        try:
+            resp, content = httplib2.Http().request(URL)
+            if resp.status==201:
+                response="Job successfully triggered"
+                dispatcher.utter_message(response)
+                dispatcher.utter_message(str(content.decode('utf-8')))
+            else:
+                dispatcher.utter_message("status code :"+str(resp.status))
+                dispatcher.utter_message(content.decode())
+        except:
+            dispatcher.utter_message("Sorry! an exception occured in triggering the job")
+        return []
+
+        
+class ActionStartJenkinsBuildWithParams(Action):
+    def name(self):
+        return "action_jenkins_param"
+
+    def run(self, dispatcher, tracker, domain):
+        job = tracker.get_slot("app_name")
+        if(tracker.get_slot("param")=="environment"):
+            param = "Environment"
+        pvalue = (tracker.get_slot("pvalue")).upper()
+        URL = "http://localhost:8080/job/"+job+"/buildWithParameters?token=remote_enable_token&"+param+"="+pvalue
+        try:
+            resp, content = httplib2.Http().request(URL)
+            if resp.status==201:
+                response="Job successfully triggered with "+param+"="+pvalue
+                dispatcher.utter_message(response)
+                dispatcher.utter_message(str(content.decode('utf-8')))
+            else:
+                dispatcher.utter_message("status code :"+str(resp.status))
+                dispatcher.utter_message(content.decode())
+        except:
+            dispatcher.utter_message("Sorry! an exception occured in triggering the job")
+        return []        
---
+++ stories.md
@@ -0,0 +1,95 @@
+## Generated Story -7546783482923213326
+* greet
+    - utter_greet
+* st_jenkin{"jenkins": " jenkins"}
+    - slot{"jenkins": " jenkins"}
+    - utter_ask_appname
+* inform
+    - utter_ask_appname
+* inform{"app_name": "test_job"}
+    - slot{"app_name": "test_job"}
+    - action_jenkins
+    - export
+
+## Generated Story 2317895387566703737
+* greet
+    - utter_greet
+* st_jenkin{"jenkins": " jenkins"}
+    - slot{"jenkins": " jenkins"}
+    - utter_ask_appname
+* inform{"app_name": "test_job"}
+    - slot{"app_name": "test_job"}
+* inform{"app_name": "test_job"}
+    - slot{"app_name": "test_job"}
+    - action_jenkins
+* goodbye
+    - utter_bye
+    - export
+
+## Generated Story 1970409352617774594
+* greet
+    - utter_greet
+* st_jenkin{"jenkins": " jenkins"}
+    - slot{"jenkins": " jenkins"}
+    - utter_ask_appname
+* inform{"app_name": "test_job"}
+    - slot{"app_name": "test_job"}
+    - action_jenkins
+* goodbye
+    - utter_bye
+    - export
+
+## Generated Story -7410773716680088475
+* greet
+    - utter_greet
+* st_jenkinp{"jenkins": " jenkins", "with_param": "with parameters"}
+    - slot{"jenkins": " jenkins"}
+    - slot{"with_param": "with parameters"}
+    - utter_ask_appname
+* inform{"app_name": "sampleapp"}
+    - slot{"app_name": "sampleapp"}
+    - utter_ask_param
+* inform_para{"param": "environment", "pvalue": "uat"}
+    - slot{"param": "environment"}
+    - slot{"pvalue": "uat"}
+    - action_jenkins_param
+* goodbye
+    - utter_bye
+    - export
+
+## Generated Story 5603926427979398985
+* greet
+    - utter_greet
+* st_jenkinp{"jenkins": " jenkins", "with_param": "with parameters"}
+    - slot{"jenkins": " jenkins"}
+    - slot{"with_param": "with parameters"}
+    - utter_ask_appname
+* inform{"app_name": "sampleapp"}
+    - slot{"app_name": "sampleapp"}
+    - utter_ask_param
+* inform_para{"param": "environment", "pvalue": "uat"}
+    - slot{"param": "environment"}
+    - slot{"pvalue": "uat"}
+    - action_jenkins_param
+* goodbye
+    - utter_bye
+    - export
+
+## Generated Story 2507783292367931614
+* greet
+    - utter_greet
+* st_jenkinp{"jenkins": " jenkins", "with_param": "with parameters"}
+    - slot{"jenkins": " jenkins"}
+    - slot{"with_param": "with parameters"}
+    - utter_ask_appname
+* inform{"app_name": "sampleapp"}
+    - slot{"app_name": "sampleapp"}
+    - utter_ask_param
+* inform_para{"param": "environment", "pvalue": "uat"}
+    - slot{"param": "environment"}
+    - slot{"pvalue": "uat"}
+    - action_jenkins_param
+* goodbye
+    - utter_bye
+    - export
+
